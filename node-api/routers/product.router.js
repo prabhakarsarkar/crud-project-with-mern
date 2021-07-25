3e4ad71e00610ee5fd7700aeb58efe073e13c62e
yousuf hassan <yousufhassan377@gmail.com>
2021-07-25 12:52:32 +0530

    ff

--- node-api/routers/product.router.js
+++ node-api/routers/product.router.js
@@ -2,9 +2,9 @@ const express = require("express");
 const productController  = require("../controllers/product.controller");
 const router = express.Router();
 const checkAuth = require('../middlewares/check-auth');
-router.post("/",productController.createProduct)
-router.get("/",productController.getAllProduct)
-router.patch("/:_id",productController.updateproduct)
-router.delete("/:_id",productController.deleteProduct)
+router.post("/",checkAuth,productController.createProduct)
+router.get("/",checkAuth,productController.getAllProduct)
+router.patch("/:_id",checkAuth,productController.updateproduct)
+router.delete("/:_id",checkAuth,productController.deleteProduct)
 module.exports= router;
 
